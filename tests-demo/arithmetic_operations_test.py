40ebf548ed147e185b4d1b3e9e97a547744cb557
Abdulaziz Ali <variant.abdulaziz@gmail.com>
2023-03-27 08:42:18 +0300

    demo tests

---
+++ tests-demo/arithmetic_operations_test.py
@@ -0,0 +1,22 @@
+'test_arthimetic_operations.py'
+import pytest
+from arithmetic_operations import ArthimeticClass
+
+class TestArithmeticClass:
+    def test_add(self ):
+        # arrange
+        x, y = 1.0, 2.0
+        expected_output = 3.0
+        # act
+        actual_output = ArthimeticClass.add(x, y)
+        # assert
+        with pytest.raises(TypeError):
+            ArthimeticClass.add("1", 2)
+        with pytest.raises(TypeError):
+            ArthimeticClass.add(1, "2")
+        with pytest.raises(TypeError):
+            ArthimeticClass.add("1", "2")
+        
+        assert expected_output == actual_output
+        
+       
