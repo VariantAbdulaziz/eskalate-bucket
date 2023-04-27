924ee971a12b85528f33163a4ee39775e437237d
Abdulaziz Ali <variant.abdulaziz@gmail.com>
2023-04-27 07:05:07 +0300

    do infrastructure layer

--- HR.LeaveManagement/src/Core/HR.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
+++ HR.LeaveManagement/src/Core/HR.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Infrastructure;
 using HR.LeaveManagement.Application.Contracts.Persistence;
 using HR.LeaveManagement.Application.DTOs.LeaveRequest;
 using HR.LeaveManagement.Application.DTOs.LeaveRequest.Validators;
@@ -6,6 +7,7 @@ using HR.LeaveManagement.Application.DTOs.LeaveType;
 using HR.LeaveManagement.Application.DTOs.LeaveType.Validators;
 using HR.LeaveManagement.Application.Exceptions;
 using HR.LeaveManagement.Application.Features.LeaveRequests.Requests.Commands;
+using HR.LeaveManagement.Application.Models;
 using HR.LeaveManagement.Domain;
 using MediatR;
 using System;
@@ -20,13 +22,18 @@ internal class CreateLeaveRequestCommandHandler : IRequestHandler<CreateLeaveReq
     private readonly ILeaveRequestRepository _leaveRequestRepository;
     private readonly IMapper _mapper;
     private readonly ILeaveTypeRepository _leaveTypeRepository;
+    private readonly IEmailSender _emailSender;
 
-    public CreateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository,
-        IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    public CreateLeaveRequestCommandHandler(
+        ILeaveRequestRepository leaveRequestRepository,
+        IMapper mapper,
+        ILeaveTypeRepository leaveTypeRepository,
+        IEmailSender emailSender)
     {
         _leaveRequestRepository = leaveRequestRepository;
         _mapper = mapper;
         _leaveTypeRepository = leaveTypeRepository;
+        _emailSender = emailSender;
     }
     public async Task<int> Handle(CreateLeaveRequestCommand request, CancellationToken cancellationToken)
     {
@@ -38,6 +45,19 @@ internal class CreateLeaveRequestCommandHandler : IRequestHandler<CreateLeaveReq
         }
         var leaveRequest = _mapper.Map<LeaveRequest>(request.LeaveRequestDto);
         leaveRequest = await _leaveRequestRepository.Add(leaveRequest);
+
+        var email = new Email
+        {
+            // TODO: initialize email body
+        };
+        try
+        {
+            await _emailSender.SendEmail(email);
+        }
+        catch (Exception ex)
+        {
+            // TODO: handle in case of failure
+        }
         return leaveRequest.Id;
     }
 }
---
+++ HR.LeaveManagement/src/Infrastructure/HR.LeaveManagement.Persistence/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,17 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence.Repositories;
+public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
+{
+    public readonly LeaveManagementDbContext _dbContext;
+    public LeaveTypeRepository(LeaveManagementDbContext dbContext) : base(dbContext)
+    {
+        _dbContext = dbContext;
+    }
+}
